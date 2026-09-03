124a1879439a1dd20534a3852bebdf411aee43eb
Divasdev <sharmadivas881@gmail.com>
2026-09-03 21:41:02 +0530

    feat(router): configure React Router v6 BrowserRouter with /auth and /auth/callback routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
 
 import App from './App';
+import { AuthCallback } from './pages/AuthCallback';
+import { AuthPage } from './pages/AuthPage';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-    <Analytics />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/auth/callback" element={<AuthCallback />} />
+      </Routes>
+      <Analytics />
+    </BrowserRouter>
+  </React.StrictMode>
 );
